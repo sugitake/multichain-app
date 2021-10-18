27cdd6aca8afa626c8d41c4051e0b9c1ba70e1fe
sugitake <jorker.jack11@gmail.com>
2021-10-18 23:17:32 +0900

    some changes

--- main.py
+++ main.py
@@ -5,25 +5,33 @@ import json
 
 
 # addresse=17vCUo38rLsCwhSJt5i3EtfbRYSVxvDZPYFJ2c
-client = mcrpc.RpcClient("127.0.0.1", "7748", "multichainrpc",
-                         "F4rnMKe6rnpYEJuqgt7bjEyo8tcsipPe6tb2pXjRmfmq")
+# client = mcrpc.RpcClient("127.0.0.1", "7748", "multichainrpc","F4rnMKe6rnpYEJuqgt7bjEyo8tcsipPe6tb2pXjRmfmq")
 # print(client.getinfo())
 # client.issue("17vCUo38rLsCwhSJt5i3EtfbRYSVxvDZPYFJ2c","asset1",100)
 # print(client.listassets())
 
 
-# def vote(vaddress, caddress):
-#     client = mcrpc.RpcClient("127.0.0.1", "7748", "multichainrpc", vaddress)
-#     result=client.sendassetfrom(vaddress, caddress, "vote_point", 1)
-#     print(result)
+def vote(vaddress, caddress, ruser, rpass):
+    client = mcrpc.RpcClient("127.0.0.1", "7748", ruser, rpc_pass)
+    result=client.sendassetfrom(vaddress, caddress, "vote_point", 1)
+    print(result)
 
-# def viewVote(vaddress):
-#     client = mcrpc.RpcClient("127.0.0.1", "7748", "multichainrpc", vaddress)
-#     result=client.getaddressbalances(vaddress)
-#     print(result)
-#     return result
+def viewVote(vaddress, ruser, rpass):
+    client = mcrpc.RpcClient("127.0.0.1", "7748",  ruser, rpc_pass)
+    result=client.getaddressbalances(vaddress)
+    print(result)
+    return result
 
-# voter_address="7FRhAxf9iGNqVXwDJNWuVmwzEGgd1Bd1LFwcpnXcMeJw"
-# candidate_address=""
-# vote(voter_address, candidate_address)
-# viewVote(voter_address)
+
+rpc_user="multichainrpc"
+rpc_pass="F4rnMKe6rnpYEJuqgt7bjEyo8tcsipPe6tb2pXjRmfmq"
+
+# server04
+voter_address="1MgT29yYwNvMCxoLNjeC7adDrc2G16qfqjYg7q"
+
+# server01
+candidate_address="1KskJXTqNtsE4sLP5maQ6DJvTFYHqLn9fkPZY8"
+
+vote(voter_address, candidate_address, rpc_user, rpc_pass)
+viewVote(voter_address)
+
